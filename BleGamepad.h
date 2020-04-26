fd0321c29e0b21ec8202392894fcaa41ee072bcb
Vladimir Dronnikov <dronnikov@gmail.com>
2020-04-26 06:57:15 +0300

    Update BleGamepad.h

--- BleGamepad.h
+++ BleGamepad.h
@@ -54,6 +54,8 @@ public:
   uint8_t batteryLevel;
   std::string deviceManufacturer;
   std::string deviceName;
+protected:
+  virtual void onStarted(BLEServer *pServer) { };
 };
 
 #endif // CONFIG_BT_ENABLED
