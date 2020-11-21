34fb08f70e8ea272260424f27c0181f3cf2ca65b
lemmingDev <jadewilliams77@hotmail.com>
2020-11-22 08:49:08 +1000

    Updated examples and Readme
    The examples and readme treated R/L triggers as signed chars, however they are now correctly referenced as chars (0 ~ 255)
    
    The HID descriptor was correct, but now people using the library will have a better chance of using the triggers correctly ;)

--- examples/Gamepad/Gamepad.ino
+++ examples/Gamepad/Gamepad.ino
@@ -2,13 +2,13 @@
  * This example turns the ESP32 into a Bluetooth LE gamepad that presses buttons and moves axis
  * 
  * Possible buttons are:
- * BUTTON_1 through to BUTTON_14* 
+ * BUTTON_1 through to BUTTON_14 
  * 
  * Possible DPAD/HAT switch position values are: 
  * DPAD_CENTERED, DPAD_UP, DPAD_UP_RIGHT, DPAD_RIGHT, DPAD_DOWN_RIGHT, 
  * DPAD_DOWN, DPAD_DOWN_LEFT, DPAD_LEFT, DPAD_UP_LEFT
  * 
- * bleGamepad.setAxes takes the following signed char parameters: 
+ * bleGamepad.setAxes takes the following signed char parameters for the Left/Right Thumb X/Y, char for the Left/Right Triggers, and hat switch position as above: 
  * (Left Thumb X, Left Thumb Y, Right Thumb X, Right Thumb Y, Left Trigger, Right Trigger, Hat switch position);
  */
  
@@ -25,14 +25,14 @@ void setup() {
 void loop() {
   if(bleGamepad.isConnected()) {
     Serial.println("Press buttons 1 and 14. Move all axes to max. Set DPAD to down right.");
-    bleGamepad.press(BUTTON_14);
     bleGamepad.press(BUTTON_1);
-    bleGamepad.setAxes(127, 127, 127, 127, 127, 127, DPAD_DOWN_RIGHT);
+    bleGamepad.press(BUTTON_14);
+    bleGamepad.setAxes(127, 127, 127, 127, 255, 255, DPAD_DOWN_RIGHT);
     delay(500);
 
-    Serial.println("Release button 14. Move all axes to min. Set DPAD to centred.");
-    bleGamepad.release(BUTTON_14);
-    bleGamepad.setAxes(-127, -127, -127, -127, -127, -127, DPAD_CENTERED);
+    Serial.println("Release button 1. Move all axes to min. Set DPAD to centred.");
+    bleGamepad.release(BUTTON_1);
+    bleGamepad.setAxes(-127, -127, -127, -127, 0, 0, DPAD_CENTERED);
     delay(500);
   }
 }
