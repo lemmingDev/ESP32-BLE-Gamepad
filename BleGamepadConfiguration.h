431a8d941439ec3cce1eb23507129cca97735560
Hagedoorn, J.P. (pieter) <j.p.hagedoorn@students.uu.nl>
2022-04-26 14:50:05 +0200

    small commit so i can continue on my other pc

--- BleGamepadConfiguration.h
+++ BleGamepadConfiguration.h
@@ -1,4 +1,21 @@
 #ifndef ESP32_BLE_GAMEPAD_CONFIG_H
 #define ESP32_BLE_GAMEPAD_CONFIG_H
 
+class BleGamepadConfiguration
+{
+private:
+    bool _autoReport;
+	uint16_t _buttonCount;
+	uint8_t _specialButtonCount;
+	uint8_t _hatSwitchCount;
+	bool _includeSpecialButton[8];
+    bool _includeAxis[8];
+    bool _includeSimulation[5];
+
+public:
+    bool autoReport();
+    uint16_t _buttonCount();
+    uint16_t 
+}
+
 #endif
