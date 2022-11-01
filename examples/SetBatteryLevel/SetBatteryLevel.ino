af97682c4e6da31bf5abd8ab4311636714fe6fc2
lemmingDev <jadewilliams77@hotmail.com>
2022-11-02 06:43:49 +1000

    Set battery level example added

---
+++ examples/SetBatteryLevel/SetBatteryLevel.ino
@@ -0,0 +1,31 @@
+/*
+ * This example shows how to set the battery level to be reported to the host OS
+ * 
+ * It reduces the power by 1% every 30 seconds
+ * 
+ */
+
+#include <Arduino.h>
+#include <BleGamepad.h>
+
+BleGamepad bleGamepad;
+
+int batteryLevel = 100;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+    bleGamepad.begin();
+}
+
+void loop()
+{
+    if (bleGamepad.isConnected())
+    {
+        Serial.print("Battery Level Set To: ");
+        Serial.println(batteryLevel);
+        bleGamepad.setBatteryLevel(batteryLevel);
+        delay(30000);
+    }
+}
