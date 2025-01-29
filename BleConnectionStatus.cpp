87880ec4a701eca456c37e23982894a02a81a630
lemmingDev <jadewilliams77@hotmail.com>
2025-01-29 21:35:21 +0900

    Update BleConnectionStatus.cpp - formatting

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -9,25 +9,25 @@ void BleConnectionStatus::onConnect(NimBLEServer *pServer, NimBLEConnInfo& connI
 {
     pServer->updateConnParams(connInfo.getConnHandle(), 6, 7, 0, 600);
 	
-	//NimBLEAddress addr = connInfo.getAddress();
+    //NimBLEAddress addr = connInfo.getAddress();
     //Serial.print("onConnect - Connected from: ");
     //Serial.println(addr.toString().c_str());
 }
 
 void BleConnectionStatus::onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason)
 {
-	//NimBLEAddress addr = connInfo.getAddress();
+    //NimBLEAddress addr = connInfo.getAddress();
     //Serial.print("onDisconnect - Disconnected from: ");
     //Serial.println(addr.toString().c_str());
 	
     this->connected = false;
-	delay(500);
-	NimBLEDevice::startAdvertising();  // Restart advertising
+    delay(500);
+    NimBLEDevice::startAdvertising();  // Restart advertising
 }
 
 void BleConnectionStatus::onAuthenticationComplete(NimBLEConnInfo& connInfo)
 {
-	//NimBLEAddress addr = connInfo.getAddress();
+    //NimBLEAddress addr = connInfo.getAddress();
     //Serial.print("onAuthenticationComplete - Connected from: ");
     //Serial.println(addr.toString().c_str());
 	
